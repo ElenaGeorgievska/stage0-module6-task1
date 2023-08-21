5f8a77da816337b13b550dc517724605b4062bf1
ElenaGeorgievska <elena_science@yahoo.com>
2023-08-22 00:40:37 +0200

    stage0_module6_task1 completed

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,49 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public void setNumberOfPaws(int numberOfPaws) {
+        this.numberOfPaws = numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public void setHasFur(boolean hasFur) {
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        if (getNumberOfPaws() == 1) {
+            return "This animal is mostly " + getColor() + "." + " It has " + getNumberOfPaws() + " paw and a fur.";
+        } else if (getNumberOfPaws() > 1 && isHasFur()) {
+            return "This animal is mostly " + getColor() + "." + " It has " + getNumberOfPaws() + " paws and a fur.";
+        } else {
+            return "This animal is mostly " + getColor() + "." + " It has " + getNumberOfPaws() + " paws and no fur.";
+
+        }
+
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,24 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+
+    public Bird() {
+        super("blue", 2, false);
+
+    }
+
+    @Override
+    public String getDescription() {
+        //return super.getDescription();
+
+        if (getNumberOfPaws() == 1) {
+            return "This animal is mostly " + getColor() + "." + " It has " + getNumberOfPaws() + " paw and a fur.";
+        } else if (getNumberOfPaws() > 1 && isHasFur()) {
+            return "This animal is mostly " + getColor() + "." + " It has " + getNumberOfPaws() + " paws and a fur.";
+        } else {
+            return "This animal is mostly " + getColor() + "." + " It has " + getNumberOfPaws() + " paws and no fur. Moreover, it has 2 wings and can fly.";
+
+        }
+    }
 }
